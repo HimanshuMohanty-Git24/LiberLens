9f43767cc78c04a78a9e845f4b99eecf76686f77
Himanshu Mohanty <codehimanshu24@gmail.com>
2024-09-24 20:22:59 +0530

    Fix app.py to run Flask server on specified host and port

--- backend/app.py
+++ backend/app.py
@@ -150,8 +150,8 @@ def ask():
 # def hello():
 #     return "Hello, Flask!"
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=8000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
 
 if __name__ == '__main__':
     app.run(debug=True)
